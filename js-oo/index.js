046cbd28647bc37eb48abfce378bd4340f196293
Mateus Castro <matcastro2004@yahoo.com.br>
2022-02-03 14:48:31 -0300

    metodos this e retornos

--- js-oo/index.js
+++ js-oo/index.js
@@ -1,18 +1,41 @@
 class Cliente {
-    constructor(){
+    nome
+    cpf
+}
+
+class ContaCorrente{
+    agencia
+    _saldo = 0
+
+    sacar(valor) {
+        if(this._saldo >= valor){
+            this._saldo -= valor
+            return valor
+        }
+        return 0
+    }
+
+    depositar(valor) {
+        if(valor <= 0){
+            return
+        }
+        this._saldo += valor
     }
 }
 
 const cliente1 = new Cliente()
 cliente1.nome = "Mateus"
 cliente1.cpf = 12345678910
-cliente1.agencia = 1001
-cliente1.saldo = 0
+
 
 const cliente2 = new Cliente()
 cliente2.nome = "Pessoa2"
 cliente2.cpf = 10987654321
-cliente2.agencia = 1001
-cliente2.saldo = 0
 
-console.log(cliente1, cliente2)
+const contaCorrenteMateus = new ContaCorrente()
+contaCorrenteMateus.agencia = 1001
+
+contaCorrenteMateus.depositar(100)
+const valorSacado = contaCorrenteMateus.sacar(50)
+console.log(valorSacado)
+console.log(contaCorrenteMateus)
