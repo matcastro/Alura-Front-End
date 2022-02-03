ce518f7e532e8502b36b7521089f022dbf4c092e
Mateus Castro <matcastro2004@yahoo.com.br>
2022-02-03 12:06:37 -0300

    operações, conversões e variáveis const e let

---
+++ js/5-atribuicaoDeVariaveis.js
@@ -0,0 +1,15 @@
+console.log("Trabalhando com atribuição de variáveis")
+//JS é case sensitive
+
+const idade = 29
+const primeiroNome = "Mateus"
+const sobrenome = "Castro"
+
+//console.log(nome + " " + sobrenome)
+console.log(primeiroNome, sobrenome)
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`)
+
+let contador = 0
+contador = contador + 1
+const nomeCompleto = primeiroNome + sobrenome
+console.log(nomeCompleto)
