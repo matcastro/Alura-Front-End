0933a7e67cc9ab36c2b1b73bd752ca7e91ab5be5
Mateus Castro <matcastro2004@yahoo.com.br>
2022-02-03 15:13:03 -0300

    modulos

--- js-oo/index.js
+++ js-oo/index.js
@@ -1,32 +1,9 @@
-class Cliente {
-    nome
-    cpf
-}
-
-class ContaCorrente{
-    agencia
-    _saldo = 0
-
-    sacar(valor) {
-        if(this._saldo >= valor){
-            this._saldo -= valor
-            return valor
-        }
-        return 0
-    }
-
-    depositar(valor) {
-        if(valor <= 0){
-            return
-        }
-        this._saldo += valor
-    }
-}
+import {Cliente} from './Cliente.js'
+import {ContaCorrente} from './ContaCorrente.js'
 
 const cliente1 = new Cliente()
 cliente1.nome = "Mateus"
 cliente1.cpf = 12345678910
-
 
 const cliente2 = new Cliente()
 cliente2.nome = "Pessoa2"
@@ -34,8 +11,16 @@ cliente2.cpf = 10987654321
 
 const contaCorrenteMateus = new ContaCorrente()
 contaCorrenteMateus.agencia = 1001
+contaCorrenteMateus.cliente = cliente1
 
 contaCorrenteMateus.depositar(100)
 const valorSacado = contaCorrenteMateus.sacar(50)
+
+const conta2 = new ContaCorrente()
+conta2.agencia = 1001
+conta2.cliente = cliente2
+
+contaCorrenteMateus.transferir(10, conta2)
+
 console.log(valorSacado)
-console.log(contaCorrenteMateus)
+console.log(contaCorrenteMateus, conta2)
